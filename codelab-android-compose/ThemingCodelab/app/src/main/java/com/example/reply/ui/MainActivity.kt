ace3d450fc5a55a55a2722db16066bf5afd42894
INGofZaun <77598028+INGofZaun@users.noreply.github.com>
2024-10-07 14:40:46 -0600

    Agregadas los colores,temas entre otras cosas para poder trabajar con colores

--- codelab-android-compose/ThemingCodelab/app/src/main/java/com/example/reply/ui/MainActivity.kt
+++ codelab-android-compose/ThemingCodelab/app/src/main/java/com/example/reply/ui/MainActivity.kt
@@ -1,18 +1,4 @@
-/*
- * Copyright 2022 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 
 package com.example.reply.ui
 
@@ -47,23 +33,34 @@ class MainActivity : ComponentActivity() {
                     viewModel.setSelectedEmail(emailId)
                 }
             )
+
+        }
+    }
+    @Preview(
+        uiMode = UI_MODE_NIGHT_YES,
+        name = "DefaultPreviewDark"
+    )
+    @Preview(
+        uiMode = UI_MODE_NIGHT_NO,
+        name = "DefaultPreviewLight"
+    )
+    @Composable
+    fun ReplyAppPreviewLight() {
+        ReplyApp(
+            replyHomeUIState = ReplyHomeUIState(
+                emails = LocalEmailsDataProvider.allEmails
+            )
+        )
+    }
+
+
+    AppTheme {
+        Surface(tonalElevation = 5.dp) {
+            ReplyApp(
+                replyHomeUIState = uiState,
+                // other parameters
+            )
         }
     }
 }
 
-@Preview(
-    uiMode = UI_MODE_NIGHT_YES,
-    name = "DefaultPreviewDark"
-)
-@Preview(
-    uiMode = UI_MODE_NIGHT_NO,
-    name = "DefaultPreviewLight"
-)
-@Composable
-fun ReplyAppPreviewLight() {
-    ReplyApp(
-        replyHomeUIState = ReplyHomeUIState(
-            emails = LocalEmailsDataProvider.allEmails
-        )
-    )
-}

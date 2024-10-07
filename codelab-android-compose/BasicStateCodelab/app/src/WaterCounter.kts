96711c8a0987eb93c0fec8df6b789bb645cf5597
INGofZaun <77598028+INGofZaun@users.noreply.github.com>
2024-10-07 13:48:02 -0600

    Ya mantiene el estado aun cuando rotas la pantalla

--- codelab-android-compose/BasicStateCodelab/app/src/WaterCounter.kts
+++ codelab-android-compose/BasicStateCodelab/app/src/WaterCounter.kts
@@ -11,22 +11,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 
+
 @Composable
-fun WaterCounter() {
-    Column(modifier = Modifier.padding(16.dp)) {
-        var count by remember { mutableStateOf(0) }
+fun WaterCounter(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        var count by rememberSaveable { mutableStateOf(0) }
         if (count > 0) {
-            var showTask by remember { mutableStateOf(true) }
-            if (showTask) {
-                WellnessTaskItem(
-                    onClose = { },
-                    taskName = "Have you taken your 15 minute walk today?"
-                )
-            }
             Text("You've had $count glasses.")
         }
-
-        Button(onClick = { count++ }, enabled = count < 10) {
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
             Text("Add one")
         }
     }
